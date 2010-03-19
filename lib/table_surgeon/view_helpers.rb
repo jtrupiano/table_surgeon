a2bec71959658d991a68a4cb090fb08111ebf0e6
John Trupiano <jtrupiano@gmail.com>
2010-03-19 14:33:02 -0400

    table_surgeon view helper displays a form and an empty table

--- lib/table_surgeon/view_helpers.rb
+++ lib/table_surgeon/view_helpers.rb
@@ -3,6 +3,15 @@ module TableSurgeon
     
     def table_surgeon(records, path)
       return no_records if records.nil? || records.empty?
+
+      # swiped from actionpack 2.3.5 form_tag_helper.rb
+      html = form_tag_html(html_options_for_form(path, :multipart => true, :method => :put))
+      html += 
+        content_tag(:table, 
+          "",
+          :class => "stickup")
+      html += "</form>".html_safe!
+      html
     end
     
     private
--- test/view_helpers_test.rb
+++ test/view_helpers_test.rb
@@ -7,6 +7,11 @@ class TableSurgeon::ViewHelpersTest < ActionController::TestCase
   include ActionView::Helpers
   include TableSurgeon::ViewHelpers
   
+  # Address rendering of form tag
+  def protect_against_forgery?
+    false
+  end
+  
   test "when an empty or nil recordset is provided, table_surgeon returns 'No records' p tag" do
     [[], nil].each do |records|
       html = table_surgeon(records, "/")
@@ -17,5 +22,12 @@ class TableSurgeon::ViewHelpersTest < ActionController::TestCase
       assert_select doc.root, "form", :count => 0
       assert_select doc.root, "table", :count => 0
     end
+  end
+  
+  test "when 4 records are provided, table_surgeon returns a form, a table with a class of 'stickup'" do
+    html = table_surgeon([1, 2, 3, 4], "/")
+    doc = HTML::Document.new html
+    assert_select doc.root, "form", :count => 1
+    assert_select doc.root, "form table.stickup", :count => 1
   end
 end
